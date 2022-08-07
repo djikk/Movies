afd5abbe0fd4bb55a3e845cd9c19c56cd388c0ad
Victor <knightivanhoe1194@gmail.com>
2022-08-07 20:31:48 +0300

    One more test for the non parameterized constructor

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -29,6 +29,29 @@ public class FilmManagerTest {
 
     @Test
 
+    public void FindLast9() {
+        FilmsManager manager = new FilmsManager(9);
+
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+
+        String[] actual = manager.findLastFilms();
+        String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2"};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+
     public void LimitIsLessThanLength() {
         FilmsManager manager = new FilmsManager(9);
 
